ec9b22407ce8898d7417ed4f96a1c224f8f96ba3
hugo luiz lazzari dos santos <37092379+katyushi@users.noreply.github.com>
2019-10-02 12:34:02 -0300

    Update ScriptBancoClube.sql

--- SqlInfo/ScriptBancoClube.sql
+++ SqlInfo/ScriptBancoClube.sql
@@ -1,4 +1,7 @@
-create database clube
+create database clube;
+
+use clube;
+
 create table socio(
 	cpf char(11) primary key,
 	nome varchar(100),
@@ -9,6 +12,7 @@ create table socio(
 	sexo char(1),
 	eciv char(1)
 );
+
 create table depende(
 	cpf char(11) primary key,
 	nome varchar(100),
@@ -17,10 +21,13 @@ create table depende(
 	dtnasc date,
 	cpf_soc char(11) 
 );
+
 alter table depende add constraint cpfSoc foreign key(cpf_soc) references socio(cpf);
+
 create table titulo(
 	cpf_soc char(11),
 	nro_tit char(6) primary key,
 	vlr_mens numeric(8,6)
 );
+
 alter table titulo add constraint cpfSoc foreign key(cpf_soc) references socio(cpf);
